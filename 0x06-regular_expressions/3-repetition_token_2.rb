f4ac4abc77343c35a658c33cc952fc78cd26d0ad
Hassan Cissey <hcissey0@gmail.com>
2023-08-02 00:05:31 +0000

    Repetition token #3

---
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -0,0 +1,4 @@
+#!/usr/bin/env ruby
+# this regex expands the *
+
+puts ARGV[0].scan(/hbt*n/).join
---
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -0,0 +1,4 @@
+#!/usr/bin/env ruby
+# this regex matches a repetition
+
+puts ARGV[0].scan(/hbt*n/).join
