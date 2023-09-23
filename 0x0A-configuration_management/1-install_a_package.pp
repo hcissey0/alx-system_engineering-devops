cc7af6cf540d1f666ec51591e9a82dcb7c55682d
Hassan Cissey <hcissey0@gmail.com>
2023-09-23 03:21:24 +0000

    Install a package

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,11 @@
+# This puppet manifest is used to install a package
+
+package { 'python3-pip':
+  ensure => installed,
+}
+
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
+}
