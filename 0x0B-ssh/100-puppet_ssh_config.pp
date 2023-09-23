5ce7e7d8ec624c93498e25d74f5bff4cd9d3fa66
Hassan Cissey <hcissey0@gmail.com>
2023-09-23 07:19:54 +0000

    Client Configuration file (w/Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,13 @@
 # This is a puppet manifest to configure ssh
 
-file_line { 'Delare Identity File':
-  path  => '/etc/ssh/ssh_config',
-  line  => '  IdentityFile ~/.ssh/school',
-  match => '^#?\\s*IdentityFile',
-}
-
-file_line { 'Turn off password authentication':
+file_line { 'Turn off passwd auth':
   path  => '/etc/ssh/ssh_config',
   line  => '  PasswordAuthentication no',
   match => '^#?\\s*PasswordAuthentication',
+}
+
+file_line { 'Delare identity file':
+  path  => '/etc/ssh/ssh_config',
+  line  => '  IdentityFile ~/.ssh/school',
+  match => '^#?\\s*IdentityFile',
 }
