5a163d4993f75834bbe876f147e3b2ba12068b4d
Hassan Cissey <hcissey0@gmail.com>
2023-09-23 05:51:19 +0000

    Create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,6 @@
 # This puppet manifest is used to create a file in /tmp
 
-file { '/tmp/school':
-  ensure  => file,
+file { '/tmp/school' :
   path    => '/tmp/school',
   mode    => '0774',
   owner   => 'www-data',
