a37c236d819d8ccf9b893b16c836a496a8f6a739
Hassan Cissey <hcissey0@gmail.com>
2023-11-10 03:50:13 +0000

    Apache

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # My puppet manifest to fix apache
 
 exec { 'fix_apache':
-  command => "/etc/init.d/apache2 restart",
+  command => '/etc/init.d/apache2 restart',
   path    => ['/bin', '/usr/bin', '/sbin', '/usr/sbin']
 }
