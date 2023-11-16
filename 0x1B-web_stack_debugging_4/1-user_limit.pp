7fde548231af9dca12cdc0e6394f670882a4d516
Hassan Cissey <hcissey0@gmail.com>
2023-11-16 01:11:53 +0000

    User Limit

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,10 +1,10 @@
 # This is a puppet manifest to grant ability to open as much files as possible
 
-exec { 'sed -i "s/holberton hard nofile 15/holberton hard nofile 5000/" /etc/security/limits.conf':
+exec { 'sed -i "s/holberton hard nofile 5/holberton hard nofile 5000/" /etc/security/limits.conf':
   path => '/usr/bin:/usr/sbin:/bin',
 }
 
-exec { 'sed -i "s/holberton soft nofile 14/holberton soft nofile 4000/" /etc/security/limits.conf':
+exec { 'sed -i "s/holberton soft nofile 4/holberton soft nofile 4000/" /etc/security/limits.conf':
   path => '/usr/bin:/usr/sbin:/bin',
 }
 
